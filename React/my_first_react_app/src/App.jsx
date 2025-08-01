82ef3b887961391e7e200367ec5a70565eab5b18
saiKelkar <kelkars1999@gmail.com>
2025-08-01 21:19:53 +0530

    Initialize React app with Vite template

---
+++ React/my_first_react_app/src/App.jsx
@@ -0,0 +1,19 @@
+const Card = ({ title }) => {
+  return (
+    <div className="card">
+      <h2>{title}</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  return(
+    <div className="card-container">
+      <Card title="Star Wars" />
+      <Card title="Avatar" />
+      <Card title="The Lion King" />
+    </div>
+  )
+}
+
+export default App
