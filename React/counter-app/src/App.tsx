0d4e2e410397903c9bf1aaadadc57df41ff68b56
saiKelkar <kelkars1999@gmail.com>
2025-08-01 22:53:29 +0530

    counter app

---
+++ React/counter-app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState<number>(0);
+
+  return(
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => setCount(prev => prev + 1)}>+</button>
+      <button onClick={() => setCount(prev => prev - 1)}>-</button>
+    </div>
+  )
+}
+
+export default App
