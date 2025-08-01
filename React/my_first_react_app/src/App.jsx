08956c97931cf56ed00c0c3cd09b8e6144db64cc
saiKelkar <kelkars1999@gmail.com>
2025-08-01 21:46:35 +0530

    Add like button and counter to Card component

--- React/my_first_react_app/src/App.jsx
+++ React/my_first_react_app/src/App.jsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from "react"
+
 const Card = ({ title }) => {
+  const [count, setCount] = useState(0);
+  const [hasLiked, setHasLiked] = useState(false);
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`)
+  }, [hasLiked])
+
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>{title} - {count || null}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? 'Liked' : 'Like'}
+      </button>
     </div>
   )
 }
 
 const App = () => {
+  
+
   return(
     <div className="card-container">
       <Card title="Star Wars" />
